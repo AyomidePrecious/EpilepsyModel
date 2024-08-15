1554ae69978ae3f19d28d8b8d618381ae5a6e3c4
AyomidePrecious <115226871+AyomidePrecious@users.noreply.github.com>
2024-08-14 19:07:43 -0700

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def main():
 
     # Load the model
     
-    model_path = "C:\Users\turningpointKS\Documents\New folder\epilepsy.sav" # Relative path
+    model_path = r"C:\Users\turningpointKS\Documents\New folder\epilepsy.sav" # Relative path
     try:
         with open(model_path, 'rb') as file:  
             loaded_model = pickle.load(file)
